35b433dc77451140b59f5a8e9912210a2f420a64
Joel Lim Zhi Qin <joel.lim@mqspectrum.com>
2017-11-27 16:00:21 +0800

    Completed recovery script, automated recovery is now functioning.

--- deploy_release/recovery.ps1
+++ deploy_release/recovery.ps1
@@ -1,6 +1,57 @@
+function recoverPortalDatabaseFolder ($destinationFolder, $portalDatabaseDestination, $portalDatabaseBackupFileLocation) {
+	echo "=================================================="
+	echo "| Initiating recovery of database folder."
+	echo "=================================================="
+	echo "Destination folder path          : $($destinationFolder)"
+	echo "Database destination folder path : $($portalDatabaseDestination)"
+	echo "Zipped file name and location    : $($portalDatabaseBackupFileLocation)"
+	$resolvedConstructedPath = resolve-path "$($destinationFolder)/$($portalDatabaseDestination)"
+	echo "Resolved constructed path        : $($resolvedConstructedPath)"
+	$currentDate = date -f yyyyMMdd_HHmmss
+	
+	rename-item "$($resolvedConstructedPath)" "$($resolvedConstructedPath)_$($currentDate)"
+	mkdir "$($resolvedConstructedPath)"
+	Add-Type -assembly "system.io.compression.filesystem"
+	[io.compression.zipfile]::ExtractToDirectory("$($portalDatabaseBackupFileLocation)", "$($resolvedConstructedPath)")
+}
 
 
 
+function recoverPortalReportingFolder ($destinationFolder, $portalReportingDestination, $portalReportingBackupFileLocation) {
+	echo "=================================================="
+	echo "| Initiating recovery of reporting folder."
+	echo "=================================================="
+	echo "Destination folder path          : $($destinationFolder)"
+	echo "Reporting destination folder path: $($portalReportingDestination)"
+	echo "Zipped file name and location    : $($portalReportingBackupFileLocation)"
+	$resolvedConstructedPath = resolve-path "$($destinationFolder)/$($portalReportingDestination)"
+	echo "Resolved constructed path        : $($resolvedConstructedPath)"
+	$currentDate = date -f yyyyMMdd_HHmmss
+	
+	rename-item "$($resolvedConstructedPath)" "$($resolvedConstructedPath)_$($currentDate)"
+	mkdir "$($resolvedConstructedPath)"
+	Add-Type -assembly "system.io.compression.filesystem"
+	[io.compression.zipfile]::ExtractToDirectory("$($portalReportingBackupFileLocation)", "$($resolvedConstructedPath)")
+}
+
+
+
+function recoverPortalPortalFolder ($destinationFolder, $portalPortalDestination, $portalPortalBackupFileLocation) {
+	echo "=================================================="
+	echo "| Initiating recovery of portal folder."
+	echo "=================================================="
+	echo "Destination folder path          : $($destinationFolder)"
+	echo "Portal destination folder path: $($portalPortalDestination)"
+	echo "Zipped file name and location    : $($portalPortalBackupFileLocation)"
+	$resolvedConstructedPath = resolve-path "$($destinationFolder)/$($portalPortalDestination)"
+	echo "Resolved constructed path        : $($resolvedConstructedPath)"
+	$currentDate = date -f yyyyMMdd_HHmmss
+	
+	rename-item "$($resolvedConstructedPath)" "$($resolvedConstructedPath)_$($currentDate)"
+	mkdir "$($resolvedConstructedPath)"
+	Add-Type -assembly "system.io.compression.filesystem"
+	[io.compression.zipfile]::ExtractToDirectory("$($portalPortalBackupFileLocation)", "$($resolvedConstructedPath)")
+}
 
 
 
@@ -8,13 +59,19 @@
 # SCRIPT START
 ################################################################################
 # Steps:
-#    Remove files from enabled components
+#    Rename files from enabled components
 #    Extract files from zipped folders
+#    We never delete the folders just to be on the safe side
+
+#iisreset /stop
+
+
 
 [xml]$deploymentDateConfigFile = get-content .\config\deploymentDateConfig.xml
 [xml]$commonConfigFile = get-content .\config\commonConfig.xml
 [xml]$releaseVersionConfigFile = get-content .\config\releaseVersionsConfig.xml
 [xml]$enabledComponentsConfigFile = get-content .\config\enabledComponentsConfig.xml
+[xml]$backupVersionsConfigFile = get-content .\config\backupVersionsConfig.xml
 
 $deploymentDate = $deploymentDateConfigFile.configuration.deploymentDate.add.value					# Deployment date.
 
@@ -37,3 +94,34 @@ $newVersion = $releaseVersionConfigFile.configuration.versionVariables.add[1].va
 $isDatabase = $enabledComponentsConfigFile.configuration.enabledComponentsVariables.add[0].value	# Is database component enabled
 $isReporting = $enabledComponentsConfigFile.configuration.enabledComponentsVariables.add[1].value	# Is reporting component enabled
 $isPortal = $enabledComponentsConfigFile.configuration.enabledComponentsVariables.add[2].value		# Is portal component enabled
+
+$portalDatabaseBackupFileLocation = $backupVersionsConfigFile.configuration.backupVariables.add[0].value
+$portalReportingBackupFileLocation = $backupVersionsConfigFile.configuration.backupVariables.add[1].value
+$portalPortalBackupFileLocation = $backupVersionsConfigFile.configuration.backupVariables.add[2].value
+
+
+
+if($isDatabase -eq "true") {
+	recoverPortalDatabaseFolder $destinationFolder $portalDatabaseVariables[1] $portalDatabaseBackupFileLocation
+} else {
+}
+
+if($isReporting -eq "true") {
+	recoverPortalReportingFolder $destinationFolder $portalReportingVariables[1] $portalReportingBackupFileLocation
+} else {
+}
+
+if($isPortal -eq "true") {
+	recoverPortalPortalFolder $destinationFolder $portalPortalVariables[1] $portalPortalBackupFileLocation
+} else {
+}
+
+Add-Type -assembly "system.io.compression.filesystem"
+
+
+
+
+#iisreset /start
+
+
+
